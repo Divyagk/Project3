9f71656651cffa8efb378f8539bef0345223d223
divya <gopalakrishnandivya6@gmail.com>
2019-03-15 12:52:02 -0600

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -101,6 +101,7 @@ class App extends Component {
                   updateUser={this.updateUser}
                 />}
             />
+           
           </Switch>
         </div>
       </BrowserRouter>
