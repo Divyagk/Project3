a639f4b98a32803bb00ebdc6d3572b48cd7e8ebb
divya <gopalakrishnandivya6@gmail.com>
2019-04-03 13:41:53 -0600

    added some images

--- client/src/components/mainpage.js
+++ client/src/components/mainpage.js
@@ -1,7 +1,7 @@
 
 
 import React from 'react';
-// import "../components/mainpage.css"
+
 // TODO - add proptypes
 
 const home = props => {
@@ -12,7 +12,7 @@ const home = props => {
 			<div className="home">
 				<img className="Pic" src={require("../images/images.jpg")} alt="coverimage" width="50%" height="400vh;"></img>
 				<img className="Pic" src={require("../images/imagess.jpg")} alt="coverimage" width="50%" height="400vh;"></img>
-				
+
 				<h1>
 
 
@@ -27,22 +27,22 @@ const home = props => {
 				<div className="col">
 					<div className="row-md-6 col-lg-4">
 						<div className="displayimages">
-					<h3>Our Products</h3>
-							<img  src={require("../images/item1.jpg")} alt="image1" width="15%" height="300vh;" ></img>
+							<h3>Our Products</h3>
+					
+							<img src={require("../images/item1.jpg")} alt="image1" width="15%" height="300vh;" ></img>
+							<img src={require("../images/item5.jpg")} alt="image2" width="15%" height="300vh;"></img>
+							<img src={require("../images/item9.jpg")} alt="image3" width="15%" height="300vh;"></img>
+							<img src={require("../images/item15.jpg")} alt="image4" width="15%" height="300vh;"></img>
+							<img src={require("../images/item17.jpg")} alt="image5" width="15%" height="300vh;"></img>
+							<img src={require("../images/item21.jpg")} alt="image5" width="15%" height="300vh;"></img>
 
-							<img  src={require("../images/item5.jpg")} alt="image2"  width="15%" height="300vh;"></img>
-							<img src={require("../images/item9.jpg")} alt="image3"  width="15%" height="300vh;"></img>
-							<img  src={require("../images/item15.jpg")} alt="image4"  width="15%" height="300vh;"></img>
-							<img src={require("../images/item17.jpg")} alt="image5"  width="15%" height="300vh;"></img>
-							<img src={require("../images/item21.jpg")} alt="image5"  width="15%" height="300vh;"></img>
-						
 						</div>
 					</div>
 
 
 				</div>
 
-				
+
 			</section>
 		</div>
 
