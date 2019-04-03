dd56ef7dbf7cf8fb1c8fb41d232ceeb38af90520
divya <gopalakrishnandivya6@gmail.com>
2019-04-03 14:09:31 -0600

    add some new links

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -7,35 +7,34 @@ const Navbar = () => {
 
     return (
         <nav className="nav-wrapper">
-                      
+
 
             <div className="container">
 
-            <img className="teamMatePic" src={require("../images/1.jpg")} width="4.4%"></img>
+                <img className="teamMatePic" src={require("../images/1.jpg")} width="4.4%"></img>
                 <Link to="/" className="brand-logo" >Fashion House</Link>
 
 
                 <ul className="right">
-            
-    
-  
-                <li><Link to="/Home">Home</Link></li>
+
+                    <li><Link to="/Home">Home</Link></li>
                     <li><Link to="/">Shop</Link></li>
                     <li><Link to="/signup">Signup</Link></li>
                     <li><Link to="/login">Login</Link></li>
+                    {/* <li><Link to="/login">About Us</Link></li> */}
                     <li><Link to="/story/:id">comment</Link></li>
-                    
-                    
+
+
                     <li><Link to="/cart">My cart</Link></li>
 
                     <li><Link to="/cart"><i className="material-icons">shopping_cart</i></Link></li>
-                    
+
                 </ul>
             </div>
         </nav>
 
 
- 
+
     )
 }
 
